ca5111f00dffc2d7af72cef51454f27949db1bce
Kaban Iklan <aboichev@gmail.com>
2025-12-05 23:22:20 -0500

    wip

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import { initStrudel } from '@strudel/web'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -11,14 +12,37 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1>Train Your Ear</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
+  <button id="play">play</button>
+  <button id="stop">stop</button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite logo to learn more
+  Click on the Vite logo to learn more
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector('#counter'))
+initStrudel({
+  prebake: () => samples('https://strudel.b-cdn.net/piano.json?strudel=1'),
+});
+
+const music = () => note("<c d e g a>*2").s('piano').play();
+const stop = () => hush();
+const drone = () => note("c2")
+  .s("sawtooth") // Use a synthesizer waveform
+  .gain(0.4)     // Set the volume
+  .attack(1)     // Slow fade in (1 second)
+  .decay(0.1)    // Quick decay to sustain level
+  .sustain(1)    // Hold at full gain until the pattern ends or changes
+  .release(1)    // Slow fade out (1 second) when the pattern stops
+  .play();
+
+//  Buttons Events
+document.getElementById('play').addEventListener('click',
+  drone
+);
+
+document.getElementById('stop').addEventListener('click',
+  stop
+);
